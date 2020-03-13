4576ba675afd6d583899037fc133871db844890c
s17025 <61729906+s17025@users.noreply.github.com>
2020-03-13 15:41:01 +0100

    1

---
+++ Cw2/Cw2/OwnComparer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+
+namespace Cw2
+{
+    class OwnComparer : IEqualityComparer<Student>
+    {
+        public bool Equals(Student x, Student y)
+        {
+            return StringComparer.InvariantCultureIgnoreCase.Equals($"{x.fname} {x.lname} {x.indexNumber}",
+                $"{y.fname} {y.lname} {y.indexNumber}");
+        }
+
+        public int GetHashCode(Student obj)
+        {
+            return StringComparer.CurrentCultureIgnoreCase.GetHashCode($"{obj.fname} {obj.lname} {obj.indexNumber}");
+        }
+    }
+}
---
+++ Cw2/Cw2/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Cw2
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            try
+            {
+                var path = @"d:\szkoła\apbd\cwiczenia02\zajęcia 2\dane.csv";
+                var stream = new StreamReader(File.OpenRead(path));
+                var lines = File.ReadLines(path);
+
+                foreach (var line in lines)
+                {
+                    var Stud = new Student
+                    {
+
+                    };
+
+                }
+            }
+            catch (FileNotFoundException)
+            {
+                Console.WriteLine("plik nie istnieje!");
+            }
+            catch (IOException)
+            {
+                throw;
+            }
+
+            var hash = new HashSet<Student>(new OwnComparer());
+
+            var stud1 = new Student
+            {
+                fname = "Jan",
+                lname = "Kowalski",
+                indexNumber = "12345"
+            };
+
+            var stud2 = new Student
+            {
+                fname = "Jan",
+                lname = "Kowalski",
+                indexNumber = "12345"
+            };
+
+            var stud3 = new Student
+            {
+                fname = "Tomasz",
+                lname = "Nowak",
+                indexNumber = "012345"
+            };
+
+            hash.Add(stud1);
+            hash.Add(stud2);
+            hash.Add(stud3);
+
+            Console.WriteLine(hash.Count);   
+
+
+        }
+    }
+}
